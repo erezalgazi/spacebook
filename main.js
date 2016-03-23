c7d900a425d9953ec740748147744f069377d2ed
erezalgazi <erezalgazi@gmail.com>
2016-03-23 17:36:59 +0200

    finished ex.4

--- main.js
+++ main.js
@@ -5,26 +5,46 @@ $('.add-post').on('click', function (e) {
 	e.preventDefault();
 	var text = $('#post-name').val();
 	var id = i;
-	i++;
 	newPost(text,id);
 	postToDiv();
-	
-	$('.remove').click(function() {
-		$(this).parent().remove();
-		var removeIndex = $(this).parent().data().id;
-		// console.log(removeIndex);
-		posts.splice(removeIndex,1);
-		// console.log(posts);
-
-		for (var x = removeIndex; x < posts.length; x++) {
-			posts[x].id = posts[x].id - 1;
-		}
-		// console.log(posts);
-
-	});
-
+	console.log(posts[i].id);
+	i++;
+	bindEvents();
 });
 
+var bindEvents = function () 
+{
+  $('.remove').click(function () {
+		$(this).parent().remove();
+  	var removeIndex = $(this).parent().data().id;
+		// var removeIndex = posts[removeIndex].id;
+		console.log(removeIndex);
+		for (x = 0; x < posts.length; x++) 
+		{
+			if (removeIndex === posts[x].id) 
+			{
+				posts.splice(x,1);
+			}
+		}
+		console.log(posts);
+		i--;
+	});
+}
+
+
+
+// $('.remove').click(function() {
+// 	var removeIndex = $(this).parent().data().id;
+// 	// console.log(removeIndex);
+// 	posts.splice(removeIndex,1);
+// 	console.log(posts);
+
+// 	for (var x = removeIndex; x < posts.length; x++) {
+// 		posts[x].id = posts[x].id - 1;
+// 	}
+// 	console.log(posts);
+// 		$(this).parent().remove();
+// });
 
 var newPost = function (text, id) {
 	var post = {
