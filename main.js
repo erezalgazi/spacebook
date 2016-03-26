b3e43acce0c2cee5dd16c99c94a3b6254967910c
erezalgazi <erezalgazi@gmail.com>
2016-03-26 15:25:55 +0300

    ongoing

--- main.js
+++ main.js
@@ -27,18 +27,27 @@ var SpacebookApp = function () {
         return posts[i];
       }
     }
-  }
+  };
 
   var createPost = function (text) {
     var post = {
       text: text,
-      id: currentId
+      id: currentId,
+      comments: []
     }
 
     currentId += 1;
 
     posts.push(post);
-  }
+  };
+
+  var createComment = function (commentText) {
+  	var comment = {
+  		text: commentText
+  	}
+  	posts[0].comments.push(comment);
+  	// console.log (posts[0]);
+  };
 
   var renderPosts = function () {
     $posts.empty();
@@ -47,14 +56,16 @@ var SpacebookApp = function () {
       var post = posts[i];
 
       var commentsContainer = '<div class="comments-container">' +
-      '<input type="text" class="comment-name">' +
-      '<button class="btn btn-primary add-comment">Post Comment</button> </div>';
+      '<input type="text" class="comment-name" placeholder="Post Comment">' +
+      '<button class="btn btn-primary add-comment">Post Comment</button> </div> <div style="margin:20px;"></div>';
 
       $posts.append('<div class="post" data-id=' + post.id + '>'
         + '<a href="#" class="remove">remove</a> ' + '<a href="#" class="show-comments">comments</a> ' + post.text +
         commentsContainer + '</div>');
     }
   }
+
+
 
   var removePost = function (currentPost) {
     var $clickedPost = $(currentPost).closest('.post');
@@ -66,27 +77,26 @@ var SpacebookApp = function () {
     $clickedPost.remove();
   }
 
-  var toggleComments = function (currentPost) {
-    var $clickedPost = $(currentPost).closest('.post');
-    $clickedPost.find('.comments-container').toggleClass('show');
-  }
+  // var toggleComments = function (currentPost) {
+  //   var $clickedPost = $(currentPost).closest('.post');
+  //   $clickedPost.find('.comments-container').toggleClass('show');
+  // }
 
   return {
     createPost: createPost,
     renderPosts: renderPosts,
     removePost: removePost,
 
-    // TODO: Implement
-    // createComment: createComment,
+    createComment: createComment
 
     // TODO: Implement
     // renderComments: renderComments,
 
     // TODO: Implement
     // removeComment: removeComment,
-    toggleComments: toggleComments
+    // toggleComments: toggleComments
   }
-}
+};
 
 var app = SpacebookApp();
 
@@ -100,12 +110,19 @@ $('.add-post').on('click', function (e) {
   var text = $('#post-name').val();
   app.createPost(text);
   app.renderPosts();
+	$('.add-comment').on('click',function(e) {
+		e.preventDefault;
+		var comment = $('.comment-name').val();
+		app.createComment(comment);
+	}); 
 });
 
 $('.posts').on('click', '.remove', function () {
   app.removePost(this);
 });
 
-$('.posts').on('click','.show-comments', function () {
-  app.toggleComments(this);
-});
+// $('.posts').on('click','.show-comments', function () {
+//   app.toggleComments(this);
+// });
+
+
